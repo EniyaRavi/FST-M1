a8ab1fe3209cc9c33d5df1085f1797804fd53ebf
EniyaRavi <eniya.ravi@ibm.com>
2025-01-01 10:53:24 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+name=input("Enter the name: ")
+age=int(input("Age is: "))
+year=str((2024-age)+100)
+print(name+ " will be turning 100 years old in the year " + year)
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+number=int(input("Number is: "))
+mod=number%2
+if mod>0:
+   print("Odd number")
+else:
+   print("Even number")  
