f11cf471d6357f1cd94ce6d4b126d9669e85ef7e
EniyaRavi <eniya.ravi@ibm.com>
2025-02-19 10:39:22 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4_DemoOne.java
@@ -0,0 +1,17 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4_DemoOne {
+	
+	@Test
+	public void firstTestCase() {
+		System.out.println("First Test Case from DemoOne class");
+	}
+	@Test
+	public void secondTestCase() {
+		System.out.println("Second Test Case from DemoOne class");
+	}
+	
+
+}
---
+++ TestNG/Activities/Activity4_DemoTwo.java
@@ -0,0 +1,11 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4_DemoTwo {
+	@Test
+	public void testCase() {
+		System.out.println("Test Case from DemoTwo class");
+	}
+
+}
